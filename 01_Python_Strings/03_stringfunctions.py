44b847f2e78c94c61a4a66a692d40ca7f4a74e3c
Jenil45 <103255558+Jenil45@users.noreply.github.com>
2022-08-10 21:19:47 +0530

    Update 03_stringfunctions.py

--- 01_Python_Strings/03_stringfunctions.py
+++ 01_Python_Strings/03_stringfunctions.py
@@ -14,3 +14,13 @@ print(story.capitalize())                    # make capital the first letter of 
 print(story.find("player"))                  # return first occurence of that written
 
 print(story.replace("once","now"))           # return replaced word string
+
+print(story.encode())                        # this method encodes the string using the specified encoding 
+
+print(story.format(time=12))                 # formats the specified value(s) and insert them inside the string's placeholder
+
+print(story.islower())                       # method returns True if all the characters are in lower case, otherwise False.
+
+print(story.split())                         # this method splits a string into a list.
+
+print(story.title())                         # in this method returns a string where the first character in every word is upper case
