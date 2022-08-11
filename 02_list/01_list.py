6dfdd683adc2da1ed393c8bcf4e0bbd127cedba7
Jenil45 <103255558+Jenil45@users.noreply.github.com>
2022-08-11 08:29:41 +0530

    Create 01_list.py

---
+++ 02_list/01_list.py
@@ -0,0 +1,14 @@
+# List are used to store value with any data type in one container
+
+# declaring list
+mylist = [45 , "Jenil" , bool , 93.18 , 7 , (12,18,24)]                         # we can store any dataype like int , string , boolean , float and tuples too.
+
+#printing the list
+print(mylist)                                                                   # will output this : [45, 'Jenil', <class 'bool'>, 93.18, 7, (12, 18, 24)]
+
+# we can do indexing of list
+# we can slice the list
+print(mylist[2:5])
+
+# we can also change or update the list
+mylist[2] = true
